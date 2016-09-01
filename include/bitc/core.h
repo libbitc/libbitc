jgarzik/picocoin

Normalize parr_* destructor conventions.  Fix a few bugs detected.

include/ccoin/core.h
@@ -70,6 +70,7 @@ extern void bp_inv_init(struct bp_inv *inv);
 extern bool deser_bp_inv(struct bp_inv *inv, struct const_buffer *buf);
 extern void ser_bp_inv(cstring *s, const struct bp_inv *inv);
 static inline void bp_inv_free(struct bp_inv *inv) {}
+extern void bp_inv_freep(void *bp_inv_p);
 
 struct bp_locator {
 	uint32_t	nVersion;
@@ -123,7 +124,7 @@ extern void bp_txin_init(struct bp_txin *txin);
 extern bool deser_bp_txin(struct bp_txin *txin, struct const_buffer *buf);
 extern void ser_bp_txin(cstring *s, const struct bp_txin *txin);
 extern void bp_txin_free(struct bp_txin *txin);
-extern void bp_txin_free_cb(void *data);
+extern void bp_txin_freep(void *data);
 static inline bool bp_txin_valid(const struct bp_txin *txin) { return true; }
 extern void bp_txin_copy(struct bp_txin *dest, const struct bp_txin *src);
 
@@ -136,7 +137,7 @@ extern void bp_txout_init(struct bp_txout *txout);
 extern bool deser_bp_txout(struct bp_txout *txout, struct const_buffer *buf);
 extern void ser_bp_txout(cstring *s, const struct bp_txout *txout);
 extern void bp_txout_free(struct bp_txout *txout);
-extern void bp_txout_free_cb(void *data);
+extern void bp_txout_freep(void *data);
 extern void bp_txout_set_null(struct bp_txout *txout);
 extern void bp_txout_copy(struct bp_txout *dest, const struct bp_txout *src);
 
@@ -166,6 +167,7 @@ extern bool deser_bp_tx(struct bp_tx *tx, struct const_buffer *buf);
 extern void ser_bp_tx(cstring *s, const struct bp_tx *tx);
 extern void bp_tx_free_vout(struct bp_tx *tx);
 extern void bp_tx_free(struct bp_tx *tx);
+extern void bp_tx_freep(void *bp_tx_p);
 extern bool bp_tx_valid(const struct bp_tx *tx);
 extern void bp_tx_calc_sha256(struct bp_tx *tx);
 extern unsigned int bp_tx_ser_size(const struct bp_tx *tx);
@@ -239,6 +241,7 @@ extern void bp_block_init(struct bp_block *block);
 extern bool deser_bp_block(struct bp_block *block, struct const_buffer *buf);
 extern void ser_bp_block(cstring *s, const struct bp_block *block);
 extern void bp_block_free(struct bp_block *block);
+extern void bp_block_freep(void *bp_block_p);
 extern void bp_block_vtx_free(struct bp_block *block);
 extern void bp_block_calc_sha256(struct bp_block *block);
 extern void bp_block_merkle(bu256_t *vo, const struct bp_block *block);

lib/core.c
@@ -67,6 +67,18 @@ void ser_bp_inv(cstring *s, const struct bp_inv *inv)
 	ser_u256(s, &inv->hash);
 }
 
+void bp_inv_freep(void *bp_inv_p)
+{
+	struct bp_inv *inv = bp_inv_p;
+	if (!inv)
+		return;
+
+	bp_inv_free(inv);
+
+	memset(inv, 0, sizeof(*inv));
+	free(inv);
+}
+
 bool deser_bp_locator(struct bp_locator *locator, struct const_buffer *buf)
 {
 	bp_locator_free(locator);
@@ -160,7 +172,7 @@ void bp_txin_free(struct bp_txin *txin)
 	}
 }
 
-void bp_txin_free_cb(void *data)
+void bp_txin_freep(void *data)
 {
 	if (!data)
 		return;
@@ -217,7 +229,7 @@ void bp_txout_free(struct bp_txout *txout)
 	}
 }
 
-void bp_txout_free_cb(void *data)
+void bp_txout_freep(void *data)
 {
 	if (!data)
 		return;
@@ -261,8 +273,8 @@ bool deser_bp_tx(struct bp_tx *tx, struct const_buffer *buf)
 {
 	bp_tx_free(tx);
 
-	tx->vin = parr_new(8, bp_txin_free_cb);
-	tx->vout = parr_new(8, bp_txout_free_cb);
+	tx->vin = parr_new(8, bp_txin_freep);
+	tx->vout = parr_new(8, bp_txout_freep);
 
 	if (!deser_u32(&tx->nVersion, buf)) return false;
 
@@ -360,6 +372,18 @@ void bp_tx_free(struct bp_tx *tx)
 	tx->sha256_valid = false;
 }
 
+void bp_tx_freep(void *p)
+{
+	struct bp_tx *tx = p;
+	if (!tx)
+		return;
+
+	bp_tx_free(tx);
+
+	memset(tx, 0, sizeof(*tx));
+	free(tx);
+}
+
 void bp_tx_calc_sha256(struct bp_tx *tx)
 {
 	if (tx->sha256_valid)
@@ -404,7 +428,7 @@ void bp_tx_copy(struct bp_tx *dest, const struct bp_tx *src)
 	else {
 		unsigned int i;
 
-		dest->vin = parr_new(src->vin->len, bp_txin_free_cb);
+		dest->vin = parr_new(src->vin->len, bp_txin_freep);
 
 		for (i = 0; i < src->vin->len; i++) {
 			struct bp_txin *txin_old, *txin_new;
@@ -421,8 +445,7 @@ void bp_tx_copy(struct bp_tx *dest, const struct bp_tx *src)
 	else {
 		unsigned int i;
 
-		dest->vout = parr_new(src->vout->len,
-						  bp_txout_free_cb);
+		dest->vout = parr_new(src->vout->len, bp_txout_freep);
 
 		for (i = 0; i < src->vout->len; i++) {
 			struct bp_txout *txout_old, *txout_new;
@@ -455,7 +478,7 @@ bool deser_bp_block(struct bp_block *block, struct const_buffer *buf)
 	if (buf->len == 0)
 		return true;
 
-	block->vtx = parr_new(512, free);
+	block->vtx = parr_new(512, bp_tx_freep);
 
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
@@ -507,23 +530,13 @@ void ser_bp_block(cstring *s, const struct bp_block *block)
 		}
 	}
 }
-
 void bp_block_vtx_free(struct bp_block *block)
 {
-	if (block && block->vtx) {
-		unsigned int i;
+	if (!block || !block->vtx)
+		return;
 
-		for (i = 0; i < block->vtx->len; i++) {
-			struct bp_tx *tx;
-
-			tx = parr_idx(block->vtx, i);
-			bp_tx_free(tx);
-		}
-
-		parr_free(block->vtx, true);
-
-		block->vtx = NULL;
-	}
+	parr_free(block->vtx, true);
+	block->vtx = NULL;
 }
 
 void bp_block_free(struct bp_block *block)
@@ -532,6 +545,18 @@ void bp_block_free(struct bp_block *block)
 		return;
 
 	bp_block_vtx_free(block);
+}
+
+void bp_block_freep(void *p)
+{
+	struct bp_block *block = p;
+	if (!block)
+		return;
+
+	bp_block_free(block);
+
+	memset(block, 0, sizeof(*block));
+	free(block);
 }
 
 void bp_block_free_cb(void *data)

lib/message.c
@@ -161,7 +161,7 @@ bool deser_msg_headers(struct msg_headers *mh, struct const_buffer *buf)
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	mh->headers = parr_new(vlen, free);
+	mh->headers = parr_new(vlen, bp_block_freep);
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -212,15 +212,6 @@ void msg_headers_free(struct msg_headers *mh)
 		return;
 
 	if (mh->headers) {
-		unsigned int i;
-
-		for (i = 0; i < mh->headers->len; i++) {
-			struct bp_block *block;
-
-			block = parr_idx(mh->headers, i);
-			bp_block_free(block);
-		}
-
 		parr_free(mh->headers, true);
 		mh->headers = NULL;
 	}
@@ -304,7 +295,7 @@ bool deser_msg_vinv(struct msg_vinv *mv, struct const_buffer *buf)
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	mv->invs = parr_new(vlen, free);
+	mv->invs = parr_new(vlen, bp_inv_freep);
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -364,7 +355,7 @@ void msg_vinv_push(struct msg_vinv *mv, uint32_t msg_type,
 		   const bu256_t *hash_in)
 {
 	if (!mv->invs)
-		mv->invs = parr_new(512, free);
+		mv->invs = parr_new(512, bp_inv_freep);
 
 	struct bp_inv *inv = malloc(sizeof(struct bp_inv));
 	inv->type = msg_type;

lib/script_eval.c
@@ -92,7 +92,7 @@ void bp_tx_sighash(bu256_t *hash, const cstring *scriptCode,
 	if ((nHashType & 0x1f) == SIGHASH_NONE) {
 		/* Wildcard payee */
 		bp_tx_free_vout(&txTmp);
-		txTmp.vout = parr_new(1, bp_txout_free_cb);
+		txTmp.vout = parr_new(1, bp_txout_freep);
 
 		/* Let the others update at will */
 		for (i = 0; i < txTmp.vin->len; i++) {
@@ -596,7 +596,7 @@ static bool bp_script_eval(parr *stack, const cstring *script,
 	struct bscript_op op;
 	bool rc = false;
 	cstring *vfExec = cstr_new(NULL);
-	parr *altstack = parr_new(0, buffer_free);
+	parr *altstack = parr_new(0, buffer_freep);
 	mpz_t bn;
 	mpz_init(bn);
 
@@ -1387,7 +1387,7 @@ bool bp_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 		      unsigned int flags, int nHashType)
 {
 	bool rc = false;
-	parr *stack = parr_new(0, buffer_free);
+	parr *stack = parr_new(0, buffer_freep);
 	parr *stackCopy = NULL;
 
 	struct const_buffer sigbuf = { scriptSig->str, scriptSig->len };
@@ -1398,7 +1398,7 @@ bool bp_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 		goto out;
 
 	if (flags & SCRIPT_VERIFY_P2SH) {
-		stackCopy = parr_new(stack->len, buffer_free);
+		stackCopy = parr_new(stack->len, buffer_freep);
 		stack_copy(stackCopy, stack);
 	}
 
@@ -1421,7 +1421,7 @@ bool bp_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 
 		cstring *pubkey2 = cstr_new_buf(pubkey2_buf->p, pubkey2_buf->len);
 
-		buffer_free(pubkey2_buf);
+		buffer_freep(pubkey2_buf);
 
 		bool rc2 = bp_script_eval(stackCopy, pubkey2, txTo, nIn,
 					  flags, nHashType);

libbitc/libbitc

include/bitc/core.h
@@ -70,6 +70,7 @@ extern void bitc_inv_init(struct bitc_inv *inv);
 extern bool deser_bitc_inv(struct bitc_inv *inv, struct const_buffer *buf);
 extern void ser_bitc_inv(cstring *s, const struct bitc_inv *inv);
 static inline void bitc_inv_free(struct bitc_inv *inv) {}
+extern void bitc_inv_freep(void *bitc_inv_p);
 
 struct bitc_locator {
 	uint32_t	nVersion;
@@ -123,7 +124,7 @@ extern void bitc_txin_init(struct bitc_txin *txin);
 extern bool deser_bitc_txin(struct bitc_txin *txin, struct const_buffer *buf);
 extern void ser_bitc_txin(cstring *s, const struct bitc_txin *txin);
 extern void bitc_txin_free(struct bitc_txin *txin);
-extern void bitc_txin_free_cb(void *data);
+extern void bitc_txin_freep(void *data);
 static inline bool bitc_txin_valid(const struct bitc_txin *txin) { return true; }
 extern void bitc_txin_copy(struct bitc_txin *dest, const struct bitc_txin *src);
 
@@ -136,7 +137,7 @@ extern void bitc_txout_init(struct bitc_txout *txout);
 extern bool deser_bitc_txout(struct bitc_txout *txout, struct const_buffer *buf);
 extern void ser_bitc_txout(cstring *s, const struct bitc_txout *txout);
 extern void bitc_txout_free(struct bitc_txout *txout);
-extern void bitc_txout_free_cb(void *data);
+extern void bitc_txout_freep(void *data);
 extern void bitc_txout_set_null(struct bitc_txout *txout);
 extern void bitc_txout_copy(struct bitc_txout *dest, const struct bitc_txout *src);
 
@@ -166,6 +167,7 @@ extern bool deser_bitc_tx(struct bitc_tx *tx, struct const_buffer *buf);
 extern void ser_bitc_tx(cstring *s, const struct bitc_tx *tx);
 extern void bitc_tx_free_vout(struct bitc_tx *tx);
 extern void bitc_tx_free(struct bitc_tx *tx);
+extern void bitc_tx_freep(void *bitc_tx_p);
 extern bool bitc_tx_valid(const struct bitc_tx *tx);
 extern void bitc_tx_calc_sha256(struct bitc_tx *tx);
 extern unsigned int bitc_tx_ser_size(const struct bitc_tx *tx);
@@ -239,6 +241,7 @@ extern void bitc_block_init(struct bitc_block *block);
 extern bool deser_bitc_block(struct bitc_block *block, struct const_buffer *buf);
 extern void ser_bitc_block(cstring *s, const struct bitc_block *block);
 extern void bitc_block_free(struct bitc_block *block);
+extern void bitc_block_freep(void *bitc_block_p);
 extern void bitc_block_vtx_free(struct bitc_block *block);
 extern void bitc_block_calc_sha256(struct bitc_block *block);
 extern void bitc_block_merkle(bu256_t *vo, const struct bitc_block *block);

lib/core.c
@@ -67,6 +67,18 @@ void ser_bitc_inv(cstring *s, const struct bitc_inv *inv)
 	ser_u256(s, &inv->hash);
 }
 
+void bitc_inv_freep(void *bitc_inv_p)
+{
+	struct bitc_inv *inv = bitc_inv_p;
+	if (!inv)
+		return;
+
+	bitc_inv_free(inv);
+
+	memset(inv, 0, sizeof(*inv));
+	free(inv);
+}
+
 bool deser_bitc_locator(struct bitc_locator *locator, struct const_buffer *buf)
 {
 	bitc_locator_free(locator);
@@ -160,7 +172,7 @@ void bitc_txin_free(struct bitc_txin *txin)
 	}
 }
 
-void bitc_txin_free_cb(void *data)
+void bitc_txin_freep(void *data)
 {
 	if (!data)
 		return;
@@ -217,7 +229,7 @@ void bitc_txout_free(struct bitc_txout *txout)
 	}
 }
 
-void bitc_txout_free_cb(void *data)
+void bitc_txout_freep(void *data)
 {
 	if (!data)
 		return;
@@ -261,8 +273,8 @@ bool deser_bitc_tx(struct bitc_tx *tx, struct const_buffer *buf)
 {
 	bitc_tx_free(tx);
 
-	tx->vin = parr_new(8, bitc_txin_free_cb);
-	tx->vout = parr_new(8, bitc_txout_free_cb);
+	tx->vin = parr_new(8, bitc_txin_freep);
+	tx->vout = parr_new(8, bitc_txout_freep);
 
 	if (!deser_u32(&tx->nVersion, buf)) return false;
 
@@ -360,6 +372,18 @@ void bitc_tx_free(struct bitc_tx *tx)
 	tx->sha256_valid = false;
 }
 
+void bitc_tx_freep(void *p)
+{
+	struct bitc_tx *tx = p;
+	if (!tx)
+		return;
+
+	bitc_tx_free(tx);
+
+	memset(tx, 0, sizeof(*tx));
+	free(tx);
+}
+
 void bitc_tx_calc_sha256(struct bitc_tx *tx)
 {
 	if (tx->sha256_valid)
@@ -404,7 +428,7 @@ void bitc_tx_copy(struct bitc_tx *dest, const struct bitc_tx *src)
 	else {
 		unsigned int i;
 
-		dest->vin = parr_new(src->vin->len, bitc_txin_free_cb);
+		dest->vin = parr_new(src->vin->len, bitc_txin_freep);
 
 		for (i = 0; i < src->vin->len; i++) {
 			struct bitc_txin *txin_old, *txin_new;
@@ -421,8 +445,7 @@ void bitc_tx_copy(struct bitc_tx *dest, const struct bitc_tx *src)
 	else {
 		unsigned int i;
 
-		dest->vout = parr_new(src->vout->len,
-						  bitc_txout_free_cb);
+		dest->vout = parr_new(src->vout->len, bitc_txout_freep);
 
 		for (i = 0; i < src->vout->len; i++) {
 			struct bitc_txout *txout_old, *txout_new;
@@ -455,7 +478,7 @@ bool deser_bitc_block(struct bitc_block *block, struct const_buffer *buf)
 	if (buf->len == 0)
 		return true;
 
-	block->vtx = parr_new(512, free);
+	block->vtx = parr_new(512, bitc_tx_freep);
 
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
@@ -507,23 +530,13 @@ void ser_bitc_block(cstring *s, const struct bitc_block *block)
 		}
 	}
 }
-
 void bitc_block_vtx_free(struct bitc_block *block)
 {
-	if (block && block->vtx) {
-		unsigned int i;
+	if (!block || !block->vtx)
+		return;
 
-		for (i = 0; i < block->vtx->len; i++) {
-			struct bitc_tx *tx;
-
-			tx = parr_idx(block->vtx, i);
-			bitc_tx_free(tx);
-		}
-
-		parr_free(block->vtx, true);
-
-		block->vtx = NULL;
-	}
+	parr_free(block->vtx, true);
+	block->vtx = NULL;
 }
 
 void bitc_block_free(struct bitc_block *block)
@@ -532,6 +545,18 @@ void bitc_block_free(struct bitc_block *block)
 		return;
 
 	bitc_block_vtx_free(block);
+}
+
+void bitc_block_freep(void *p)
+{
+	struct bitc_block *block = p;
+	if (!block)
+		return;
+
+	bitc_block_free(block);
+
+	memset(block, 0, sizeof(*block));
+	free(block);
 }
 
 void bitc_block_free_cb(void *data)

lib/message.c
@@ -161,7 +161,7 @@ bool deser_msg_headers(struct msg_headers *mh, struct const_buffer *buf)
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	mh->headers = parr_new(vlen, free);
+	mh->headers = parr_new(vlen, bitc_block_freep);
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -212,14 +212,6 @@ void msg_headers_free(struct msg_headers *mh)
 		return;
 
 	if (mh->headers) {
-		unsigned int i;
-
-		for (i = 0; i < mh->headers->len; i++) {
-			struct bitc_block *block;
-
-			block = parr_idx(mh->headers, i);
-			bitc_block_free(block);
-		}
 
 		parr_free(mh->headers, true);
 		mh->headers = NULL;
@@ -304,7 +296,7 @@ bool deser_msg_vinv(struct msg_vinv *mv, struct const_buffer *buf)
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	mv->invs = parr_new(vlen, free);
+	mv->invs = parr_new(vlen, bitc_inv_freep);
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -364,7 +356,7 @@ void msg_vinv_push(struct msg_vinv *mv, uint32_t msg_type,
 		   const bu256_t *hash_in)
 {
 	if (!mv->invs)
-		mv->invs = parr_new(512, free);
+		mv->invs = parr_new(512, bitc_inv_freep);
 
 	struct bitc_inv *inv = malloc(sizeof(struct bitc_inv));
 	inv->type = msg_type;

lib/script_eval.c
@@ -92,7 +92,7 @@ void bitc_tx_sighash(bu256_t *hash, const cstring *scriptCode,
 	if ((nHashType & 0x1f) == SIGHASH_NONE) {
 		/* Wildcard payee */
 		bitc_tx_free_vout(&txTmp);
-		txTmp.vout = parr_new(1, bitc_txout_free_cb);
+		txTmp.vout = parr_new(1, bitc_txout_freep);
 
 		/* Let the others update at will */
 		for (i = 0; i < txTmp.vin->len; i++) {
@@ -596,7 +596,7 @@ static bool bitc_script_eval(parr *stack, const cstring *script,
 	struct bscript_op op;
 	bool rc = false;
 	cstring *vfExec = cstr_new(NULL);
-	parr *altstack = parr_new(0, buffer_free);
+	parr *altstack = parr_new(0, buffer_freep);
 	mpz_t bn;
 	mpz_init(bn);
 
@@ -1387,7 +1387,7 @@ bool bitc_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 		      unsigned int flags, int nHashType)
 {
 	bool rc = false;
-	parr *stack = parr_new(0, buffer_free);
+	parr *stack = parr_new(0, buffer_freep);
 	parr *stackCopy = NULL;
 
 	struct const_buffer sigbuf = { scriptSig->str, scriptSig->len };
@@ -1398,7 +1398,7 @@ bool bitc_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 		goto out;
 
 	if (flags & SCRIPT_VERIFY_P2SH) {
-		stackCopy = parr_new(stack->len, buffer_free);
+		stackCopy = parr_new(stack->len, buffer_freep);
 		stack_copy(stackCopy, stack);
 	}
 
@@ -1421,7 +1421,7 @@ bool bitc_script_verify(const cstring *scriptSig, const cstring *scriptPubKey,
 
 		cstring *pubkey2 = cstr_new_buf(pubkey2_buf->p, pubkey2_buf->len);
 
-		buffer_free(pubkey2_buf);
+		buffer_freep(pubkey2_buf);
 
 		bool rc2 = bitc_script_eval(stackCopy, pubkey2, txTo, nIn,
 					  flags, nHashType);