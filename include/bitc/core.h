jgarzik/picocoin

[lib] Fix msg_addr mem bugs. Introduce "freep" convention for destructors.

include/ccoin/core.h
@@ -53,6 +53,7 @@ extern bool deser_bp_addr(unsigned int protover,
 		struct bp_address *addr, struct const_buffer *buf);
 extern void ser_bp_addr(cstring *s, unsigned int protover, const struct bp_address *addr);
 static inline void bp_addr_free(struct bp_address *addr) {}
+extern void bp_addr_freep(void *p);
 
 static inline void bp_addr_copy(struct bp_address *dest,
 				const struct bp_address *src)

lib/core.c
@@ -34,6 +34,19 @@ void ser_bp_addr(cstring *s, unsigned int protover, const struct bp_address *add
 	ser_u16(s, addr->port);
 }
 
+void bp_addr_freep(void *p)
+{
+	struct bp_address *addr = p;
+
+	if (!addr)
+		return;
+
+	bp_addr_free(addr);
+
+	memset(addr, 0, sizeof(*addr));
+	free(addr);
+}
+
 void bp_inv_init(struct bp_inv *inv)
 {
 	memset(inv, 0, sizeof(*inv));
@@ -85,7 +98,7 @@ void bp_locator_push(struct bp_locator *locator, const bu256_t *hash_in)
 {
 	/* TODO: replace '16' with number based on real world usage */
 	if (!locator->vHave)
-		locator->vHave = parr_new(16, bu256_free);
+		locator->vHave = parr_new(16, bu256_freep);
 
 	bu256_t *hash = bu256_new(hash_in);
 	parr_add(locator->vHave, hash);

lib/message.c
@@ -72,12 +72,14 @@ cstring *message_str(const unsigned char netmagic[4],
 bool deser_msg_addr(unsigned int protover, struct msg_addr *ma,
 		    struct const_buffer *buf)
 {
-	memset(ma, 0, sizeof(*ma));
+	msg_addr_free(ma);
 
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	ma->addrs = parr_new(vlen, free);
+	ma->addrs = parr_new(vlen, bp_addr_freep);
+	if (!ma->addrs)
+		return false;
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -124,6 +126,9 @@ cstring *ser_msg_addr(unsigned int protover, const struct msg_addr *ma)
 
 void msg_addr_free(struct msg_addr *ma)
 {
+	if (!ma)
+		return;
+
 	if (ma->addrs) {
 		parr_free(ma->addrs, true);
 		ma->addrs = NULL;

src/brd.c
@@ -516,7 +516,7 @@ static void readprep_blocks_file(void)
 static void init_orphans(void)
 {
 	orphans = bp_hashtab_new_ext(bu256_hash, bu256_equal_,
-				     (bp_freefunc) bu256_free, (bp_freefunc) buffer_free);
+				     bu256_freep, (bp_freefunc) buffer_free);
 }
 
 static bool have_orphan(const bu256_t *v)
@@ -537,7 +537,7 @@ static bool add_orphan(const bu256_t *hash_in, struct const_buffer *buf_in)
 
 	struct buffer *buf = buffer_copy(buf_in->p, buf_in->len);
 	if (!buf) {
-		bu256_free(hash);
+		bu256_freep(hash);
 		log_info("%s: OOM", prog_name);
 		return false;
 	}

test/message.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <ccoin/message.h>
+#include <ccoin/net/net.h>
 
 static void check_buffer(const cstring *buffer,
                          const void *expected_data,
@@ -305,9 +306,55 @@ static void test_version()
     }
 }
 
+static void test_addr(void)
+{
+	static const unsigned char pchLocal[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+	struct bp_address *addr;
+
+	struct msg_addr ma;
+
+	msg_addr_init(&ma);
+
+	ma.addrs = parr_new(1, bp_addr_freep);
+
+	addr = calloc(1, sizeof(*addr));
+	assert(addr != NULL);
+
+	bp_addr_init(addr);
+
+	uint32_t time32 = (uint32_t) time(NULL);
+	addr->nTime = htole32(time32);
+	memcpy(&addr->ip[0], &pchLocal[0], sizeof(addr->ip));
+	addr->port = htole32(8333);
+
+	bool rc = parr_add(ma.addrs, addr);
+	assert(rc == true);
+
+	cstring *addr_ser = ser_msg_addr(PROTO_VERSION, &ma);
+	assert(addr_ser != NULL);
+	assert(addr_ser->len > 0);
+
+	struct const_buffer buf = { addr_ser->str, addr_ser->len };
+
+	// note: overwrites 'ma' objects, freeing them
+
+	rc = deser_msg_addr(PROTO_VERSION, &ma, &buf);
+	assert(rc == true);
+	assert(ma.addrs != NULL);
+	assert(ma.addrs->len == 1);
+
+	addr = parr_idx(ma.addrs, 0);
+	assert(addr->nTime == le32toh(time32));
+	assert(memcmp(&addr->ip[0], &pchLocal[0], sizeof(addr->ip)) == 0);
+	assert(le32toh(addr->port) == 8333);
+
+	msg_addr_free(&ma);
+}
+
 int main(int argc, char **argv)
 {
     test_version();
+    test_addr();
 
     return 0;
 }

libbitc/libbitc

include/bitc/core.h
@@ -53,6 +53,7 @@ extern bool deser_bitc_addr(unsigned int protover,
 		struct bitc_address *addr, struct const_buffer *buf);
 extern void ser_bitc_addr(cstring *s, unsigned int protover, const struct bitc_address *addr);
 static inline void bitc_addr_free(struct bitc_address *addr) {}
+extern void bitc_addr_freep(void *p);
 
 static inline void bitc_addr_copy(struct bitc_address *dest,
 				const struct bitc_address *src)

lib/core.c
@@ -34,6 +34,19 @@ void ser_bitc_addr(cstring *s, unsigned int protover, const struct bitc_address 
 	ser_u16(s, addr->port);
 }
 
+void bitc_addr_freep(void *p)
+{
+	struct bitc_address *addr = p;
+
+	if (!addr)
+		return;
+
+	bitc_addr_free(addr);
+
+	memset(addr, 0, sizeof(*addr));
+	free(addr);
+}
+
 void bitc_inv_init(struct bitc_inv *inv)
 {
 	memset(inv, 0, sizeof(*inv));
@@ -85,7 +98,7 @@ void bitc_locator_push(struct bitc_locator *locator, const bu256_t *hash_in)
 {
 	/* TODO: replace '16' with number based on real world usage */
 	if (!locator->vHave)
-		locator->vHave = parr_new(16, bu256_free);
+		locator->vHave = parr_new(16, bu256_freep);
 
 	bu256_t *hash = bu256_new(hash_in);
 	parr_add(locator->vHave, hash);

lib/message.c
@@ -72,12 +72,14 @@ cstring *message_str(const unsigned char netmagic[4],
 bool deser_msg_addr(unsigned int protover, struct msg_addr *ma,
 		    struct const_buffer *buf)
 {
-	memset(ma, 0, sizeof(*ma));
+	msg_addr_free(ma);
 
 	uint32_t vlen;
 	if (!deser_varlen(&vlen, buf)) return false;
 
-	ma->addrs = parr_new(vlen, free);
+	ma->addrs = parr_new(vlen, bitc_addr_freep);
+	if (!ma->addrs)
+		return false;
 
 	unsigned int i;
 	for (i = 0; i < vlen; i++) {
@@ -124,6 +126,9 @@ cstring *ser_msg_addr(unsigned int protover, const struct msg_addr *ma)
 
 void msg_addr_free(struct msg_addr *ma)
 {
+	if (!ma)
+		return;
+
 	if (ma->addrs) {
 		parr_free(ma->addrs, true);
 		ma->addrs = NULL;

src/brd.c
@@ -516,7 +516,7 @@ static void readprep_blocks_file(void)
 static void init_orphans(void)
 {
 	orphans = bitc_hashtab_new_ext(bu256_hash, bu256_equal_,
-				     (bitc_freefunc) bu256_free, (bitc_freefunc) buffer_free);
+				     bu256_freep, (bitc_freefunc) buffer_free);
 }
 
 static bool have_orphan(const bu256_t *v)
@@ -537,7 +537,7 @@ static bool add_orphan(const bu256_t *hash_in, struct const_buffer *buf_in)
 
 	struct buffer *buf = buffer_copy(buf_in->p, buf_in->len);
 	if (!buf) {
-		bu256_free(hash);
+		bu256_freep(hash);
 		log_info("%s: OOM", prog_name);
 		return false;
 	}

test/message.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <bitc/message.h>
+#include <bitc/net/net.h>
 
 static void check_buffer(const cstring *buffer,
                          const void *expected_data,
@@ -305,9 +306,55 @@ static void test_version()
     }
 }
 
+static void test_addr(void)
+{
+	static const unsigned char pchLocal[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
+	struct bitc_address *addr;
+
+	struct msg_addr ma;
+
+	msg_addr_init(&ma);
+
+	ma.addrs = parr_new(1, bitc_addr_freep);
+
+	addr = calloc(1, sizeof(*addr));
+	assert(addr != NULL);
+
+	bitc_addr_init(addr);
+
+	uint32_t time32 = (uint32_t) time(NULL);
+	addr->nTime = htole32(time32);
+	memcpy(&addr->ip[0], &pchLocal[0], sizeof(addr->ip));
+	addr->port = htole32(8333);
+
+	bool rc = parr_add(ma.addrs, addr);
+	assert(rc == true);
+
+	cstring *addr_ser = ser_msg_addr(PROTO_VERSION, &ma);
+	assert(addr_ser != NULL);
+	assert(addr_ser->len > 0);
+
+	struct const_buffer buf = { addr_ser->str, addr_ser->len };
+
+	// note: overwrites 'ma' objects, freeing them
+
+	rc = deser_msg_addr(PROTO_VERSION, &ma, &buf);
+	assert(rc == true);
+	assert(ma.addrs != NULL);
+	assert(ma.addrs->len == 1);
+
+	addr = parr_idx(ma.addrs, 0);
+	assert(addr->nTime == le32toh(time32));
+	assert(memcmp(&addr->ip[0], &pchLocal[0], sizeof(addr->ip)) == 0);
+	assert(le32toh(addr->port) == 8333);
+
+	msg_addr_free(&ma);
+}
+
 int main(int argc, char **argv)
 {
     test_version();
+    test_addr();
 
     return 0;
 }