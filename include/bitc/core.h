jgarzik/picocoin

brd: fix leak-on-error

include/ccoin/core.h
@@ -197,6 +197,7 @@ struct bp_utxo {
 
 extern void bp_utxo_init(struct bp_utxo *coin);
 extern void bp_utxo_free(struct bp_utxo *coin);
+extern void bp_utxo_freep(void *bp_utxo_coin);
 extern bool bp_utxo_from_tx(struct bp_utxo *coin, const struct bp_tx *tx,
 		     bool is_coinbase, unsigned int height);
 

lib/utxo.c
@@ -30,6 +30,18 @@ void bp_utxo_free(struct bp_utxo *coin)
 	bp_utxo_free_vout(coin);
 }
 
+void bp_utxo_freep(void *p)
+{
+	struct bp_utxo *coin = p;
+	if (!coin)
+		return;
+
+	bp_utxo_free(coin);
+
+	memset(coin, 0, sizeof(*coin));
+	free(coin);
+}
+
 bool bp_utxo_from_tx(struct bp_utxo *coin, const struct bp_tx *tx,
 		     bool is_coinbase, unsigned int height)
 {

src/brd.c
@@ -364,8 +364,10 @@ static bool spend_tx(struct bp_utxo_set *uset, const struct bp_tx *tx,
 	coin = calloc(1, sizeof(*coin));
 	bp_utxo_init(coin);
 
-	if (!bp_utxo_from_tx(coin, tx, is_coinbase, height))
+	if (!bp_utxo_from_tx(coin, tx, is_coinbase, height)) {
+		bp_utxo_freep(coin);
 		return false;
+	}
 
 	/* add unspent outputs to set */
 	bp_utxo_set_add(uset, coin);

libbitc/libbitc

include/bitc/core.h
@@ -197,6 +197,7 @@ struct bitc_utxo {
 
 extern void bitc_utxo_init(struct bitc_utxo *coin);
 extern void bitc_utxo_free(struct bitc_utxo *coin);
+extern void bitc_utxo_freep(void *bitc_utxo_coin);
 extern bool bitc_utxo_from_tx(struct bitc_utxo *coin, const struct bitc_tx *tx,
 		     bool is_coinbase, unsigned int height);
 

lib/utxo.c
@@ -30,6 +30,18 @@ void bitc_utxo_free(struct bitc_utxo *coin)
 	bitc_utxo_free_vout(coin);
 }
 
+void bitc_utxo_freep(void *p)
+{
+	struct bitc_utxo *coin = p;
+	if (!coin)
+		return;
+
+	bitc_utxo_free(coin);
+
+	memset(coin, 0, sizeof(*coin));
+	free(coin);
+}
+
 bool bitc_utxo_from_tx(struct bitc_utxo *coin, const struct bitc_tx *tx,
 		     bool is_coinbase, unsigned int height)
 {

src/brd.c
@@ -364,8 +364,10 @@ static bool spend_tx(struct bitc_utxo_set *uset, const struct bitc_tx *tx,
 	coin = calloc(1, sizeof(*coin));
 	bitc_utxo_init(coin);
 
-	if (!bitc_utxo_from_tx(coin, tx, is_coinbase, height))
+	if (!bitc_utxo_from_tx(coin, tx, is_coinbase, height)) {
+		bitc_utxo_freep(coin);
 		return false;
+	}
 
 	/* add unspent outputs to set */
 	bitc_utxo_set_add(uset, coin);