jgarzik/picocoin

[lib] key and ext-key init need not return a value

include/ccoin/key.h
@@ -25,7 +25,7 @@ struct bp_key {
 /// Frees any internally allocated static data.
 extern void bp_key_static_shutdown();
 
-extern bool bp_key_init(struct bp_key *key);
+extern void bp_key_init(struct bp_key *key);
 extern void bp_key_free(struct bp_key *key);
 extern bool bp_key_generate(struct bp_key *key);
 extern bool bp_privkey_set(struct bp_key *key, const void *privkey, size_t pk_len);

lib/hdkeys.c
@@ -16,17 +16,10 @@
 #define TEST_PUBLIC 0x043587CF
 #define TEST_PRIVATE 0x04358394
 
-bool hd_extended_key_init(struct hd_extended_key *ek)
+void hd_extended_key_init(struct hd_extended_key *ek)
 {
-	if (bp_key_init(&ek->key)) {
-		memset(ek->chaincode.data, 0, sizeof(ek->chaincode.data));
-		ek->index = 0;
-		ek->version = 0;
-		memset(ek->parent_fingerprint, 0, 4);
-		ek->depth = 0;
-		return true;
-	}
-	return false;
+	memset(ek, 0, sizeof(*ek));
+	bp_key_init(&ek->key);
 }
 
 void hd_extended_key_free(struct hd_extended_key *ek)
@@ -203,8 +196,8 @@ bool hd_derive(struct hd_extended_key *out_child,
 		if (is_last)
 			target = out_child;
 
-		if (!hd_extended_key_init(&tmp) ||
-		    !hd_extended_key_generate_child(&parent, val, target))
+		hd_extended_key_init(&tmp);
+		if (!hd_extended_key_generate_child(&parent, val, target))
 			return false;
 
 		if (!is_last)

lib/key.c
@@ -44,10 +44,9 @@ void bp_key_static_shutdown()
 	}
 }
 
-bool bp_key_init(struct bp_key *key)
+void bp_key_init(struct bp_key *key)
 {
-	memset(key->secret, 0, sizeof(key->secret));
-	return true;
+	memset(key, 0, sizeof(*key));
 }
 
 void bp_key_free(struct bp_key *key)

lib/wallet.c
@@ -330,8 +330,7 @@ static bool load_rec_privkey(struct wallet *wlt, const void *privkey, size_t pk_
 	struct bp_key *key;
 
 	key = calloc(1, sizeof(*key));
-	if (!bp_key_init(key))
-		goto err_out;
+	bp_key_init(key);
 	if (!bp_privkey_set(key, privkey, pk_len))
 		goto err_out_kf;
 
@@ -341,7 +340,6 @@ static bool load_rec_privkey(struct wallet *wlt, const void *privkey, size_t pk_
 
 err_out_kf:
 	bp_key_free(key);
-err_out:
 	free(key);
 	return false;
 }
@@ -351,10 +349,7 @@ static bool load_rec_hdmaster(struct wallet *wlt, const void *mkey, size_t mlen)
 	struct hd_extended_key *hdkey;
 
 	hdkey = calloc(1, sizeof(*hdkey));
-	if (!hd_extended_key_init(hdkey)) {
-		fprintf(stderr, "hdmaster fail 1\n");
-		goto err_out;
-	}
+	hd_extended_key_init(hdkey);
 	if (!hd_extended_key_deser(hdkey, mkey, mlen)) {
 		fprintf(stderr, "hdmaster fail 2\n");
 		goto err_out_kf;
@@ -366,7 +361,6 @@ static bool load_rec_hdmaster(struct wallet *wlt, const void *mkey, size_t mlen)
 
 err_out_kf:
 	hd_extended_key_free(hdkey);
-err_out:
 	free(hdkey);
 	return false;
 }
@@ -480,8 +474,8 @@ bool wallet_create(struct wallet *wlt, const void *seed, size_t seed_len)
 {
 	struct hd_extended_key *hdkey;
 	hdkey = calloc(1, sizeof(*hdkey));
-	if (!hd_extended_key_init(hdkey) ||
-	    !hd_extended_key_generate_master(hdkey, seed, seed_len))
+	hd_extended_key_init(hdkey);
+	if (!hd_extended_key_generate_master(hdkey, seed, seed_len))
 		goto err_out_hdkey;
 
 	struct wallet_account *acct;

test/keyset.c
@@ -83,7 +83,7 @@ static void runtest(void)
 	/* generate keys */
 	for (i = 0; i < ARRAY_SIZE(keys); i++) {
 		struct bp_key *key = &keys[i];
-		assert(bp_key_init(key) == true);
+		bp_key_init(key);
 		assert(bp_key_generate(key) == true);
 	}
 

test/wallet-basics.c
@@ -99,7 +99,7 @@ static void runtest(const char *json_base_fn, const char *ser_in_fn,
 
 	/* load key that has received an incoming payment */
 	struct bp_key key;
-	assert(bp_key_init(&key) == true);
+	bp_key_init(&key);
 
 	load_json_key(wallet, &key);
 

libbitc/libbitc

include/bitc/key.h
@@ -25,7 +25,7 @@ struct bitc_key {
 /// Frees any internally allocated static data.
 extern void bitc_key_static_shutdown();
 
-extern bool bitc_key_init(struct bitc_key *key);
+extern void bitc_key_init(struct bitc_key *key);
 extern void bitc_key_free(struct bitc_key *key);
 extern bool bitc_key_generate(struct bitc_key *key);
 extern bool bitc_privkey_set(struct bitc_key *key, const void *privkey, size_t pk_len);

lib/hdkeys.c
@@ -16,17 +16,10 @@
 #define TEST_PUBLIC 0x043587CF
 #define TEST_PRIVATE 0x04358394
 
-bool hd_extended_key_init(struct hd_extended_key *ek)
+void hd_extended_key_init(struct hd_extended_key *ek)
 {
-	if (bitc_key_init(&ek->key)) {
-		memset(ek->chaincode.data, 0, sizeof(ek->chaincode.data));
-		ek->index = 0;
-		ek->version = 0;
-		memset(ek->parent_fingerprint, 0, 4);
-		ek->depth = 0;
-		return true;
-	}
-	return false;
+	memset(ek, 0, sizeof(*ek));
+	bitc_key_init(&ek->key);
 }
 
 void hd_extended_key_free(struct hd_extended_key *ek)
@@ -203,8 +196,8 @@ bool hd_derive(struct hd_extended_key *out_child,
 		if (is_last)
 			target = out_child;
 
-		if (!hd_extended_key_init(&tmp) ||
-		    !hd_extended_key_generate_child(&parent, val, target))
+		hd_extended_key_init(&tmp);
+		if (!hd_extended_key_generate_child(&parent, val, target))
 			return false;
 
 		if (!is_last)

lib/key.c
@@ -44,10 +44,9 @@ void bitc_key_static_shutdown()
 	}
 }
 
-bool bitc_key_init(struct bitc_key *key)
+void bitc_key_init(struct bitc_key *key)
 {
-	memset(key->secret, 0, sizeof(key->secret));
-	return true;
+	memset(key, 0, sizeof(*key));
 }
 
 void bitc_key_free(struct bitc_key *key)

lib/wallet.c
@@ -330,8 +330,7 @@ static bool load_rec_privkey(struct wallet *wlt, const void *privkey, size_t pk_
 	struct bitc_key *key;
 
 	key = calloc(1, sizeof(*key));
-	if (!bitc_key_init(key))
-		goto err_out;
+	bitc_key_init(key);
 	if (!bitc_privkey_set(key, privkey, pk_len))
 		goto err_out_kf;
 
@@ -341,7 +340,6 @@ static bool load_rec_privkey(struct wallet *wlt, const void *privkey, size_t pk_
 
 err_out_kf:
 	bitc_key_free(key);
-err_out:
 	free(key);
 	return false;
 }
@@ -351,10 +349,7 @@ static bool load_rec_hdmaster(struct wallet *wlt, const void *mkey, size_t mlen)
 	struct hd_extended_key *hdkey;
 
 	hdkey = calloc(1, sizeof(*hdkey));
-	if (!hd_extended_key_init(hdkey)) {
-		fprintf(stderr, "hdmaster fail 1\n");
-		goto err_out;
-	}
+	hd_extended_key_init(hdkey);
 	if (!hd_extended_key_deser(hdkey, mkey, mlen)) {
 		fprintf(stderr, "hdmaster fail 2\n");
 		goto err_out_kf;
@@ -366,7 +361,6 @@ static bool load_rec_hdmaster(struct wallet *wlt, const void *mkey, size_t mlen)
 
 err_out_kf:
 	hd_extended_key_free(hdkey);
-err_out:
 	free(hdkey);
 	return false;
 }
@@ -480,8 +474,8 @@ bool wallet_create(struct wallet *wlt, const void *seed, size_t seed_len)
 {
 	struct hd_extended_key *hdkey;
 	hdkey = calloc(1, sizeof(*hdkey));
-	if (!hd_extended_key_init(hdkey) ||
-	    !hd_extended_key_generate_master(hdkey, seed, seed_len))
+	hd_extended_key_init(hdkey);
+	if (!hd_extended_key_generate_master(hdkey, seed, seed_len))
 		goto err_out_hdkey;
 
 	struct wallet_account *acct;

test/keyset.c
@@ -83,7 +83,7 @@ static void runtest(void)
 	/* generate keys */
 	for (i = 0; i < ARRAY_SIZE(keys); i++) {
 		struct bitc_key *key = &keys[i];
-		assert(bitc_key_init(key) == true);
+		bitc_key_init(key);
 		assert(bitc_key_generate(key) == true);
 	}
 

test/wallet-basics.c
@@ -99,7 +99,7 @@ static void runtest(const char *json_base_fn, const char *ser_in_fn,
 
 	/* load key that has received an incoming payment */
 	struct bitc_key key;
-	assert(bitc_key_init(&key) == true);
+	bitc_key_init(&key);
 
 	load_json_key(wallet, &key);
 