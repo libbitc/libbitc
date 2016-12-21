jgarzik/picocoin

crypto: Replace all instances of OpenSSL RAND_bytes with an implementation of Fortuna PRNG

lib/crypto/prng.c
@@ -1,0 +1,91 @@
+/*
+ * prng.c
+ *		Wrapper for builtin functions
+ *
+ * Copyright (c) 2001 Marko Kreen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * contrib/pgcrypto/internal.c
+ */
+
+#include <ccoin/crypto/fortuna.h> // for fortuna_add_entropy, etc
+#include <ccoin/crypto/prng.h>
+
+#include <stdlib.h> // for NULL
+#include <string.h> // for memset
+#include <time.h>   // for time_t, time
+
+/*
+ * Randomness provider
+ */
+
+static time_t seed_time = 0;
+static time_t check_time = 0;
+
+static void system_reseed(void) {
+    uint8_t buf[1024];
+    int n;
+    time_t t;
+    int skip = 1;
+
+    t = time(NULL);
+
+    if (seed_time == 0)
+        skip = 0;
+    else if ((t - seed_time) < SYSTEM_RESEED_MIN)
+        skip = 1;
+    else if ((t - seed_time) > SYSTEM_RESEED_MAX)
+        skip = 0;
+    else if (check_time == 0 || (t - check_time) > SYSTEM_RESEED_CHECK_TIME) {
+        check_time = t;
+
+        /* roll dice */
+        prng_get_random_bytes(buf, 1);
+        skip = buf[0] >= SYSTEM_RESEED_CHANCE;
+    }
+    /* clear 1 byte */
+    memset(buf, 0, sizeof(buf));
+
+    if (skip)
+        return;
+
+    n = prng_acquire_system_randomness(buf);
+    if (n > 0)
+        fortuna_add_entropy(buf, n);
+
+    seed_time = t;
+    memset(buf, 0, sizeof(buf));
+}
+
+int prng_get_random_bytes(uint8_t *dst, unsigned count) {
+    system_reseed();
+    fortuna_get_bytes(count, dst);
+    return 0;
+}
+
+int prng_add_entropy(const uint8_t *data, unsigned count) {
+    system_reseed();
+    fortuna_add_entropy(data, count);
+    return 0;
+}

src/brd.c
@@ -10,6 +10,7 @@
 #include <ccoin/clist.h>                // for clist_length
 #include <ccoin/core.h>                 // for bp_block, bp_utxo, bp_tx, etc
 #include <ccoin/coredefs.h>             // for chain_info, chain_find, etc
+#include <ccoin/crypto/prng.h>          // for prng_get_random_bytes
 #include <ccoin/cstr.h>                 // for cstring, cstr_free
 #include <ccoin/hexcode.h>              // for decode_hex
 #include <ccoin/log.h>                  // for log_info, logging, etc
@@ -21,13 +22,13 @@
 #include <ccoin/script.h>               // for bp_verify_sig
 #include <ccoin/util.h>                 // for ARRAY_SIZE, czstr_equal, etc
 
+
 #include <assert.h>                     // for assert
 #include <stdbool.h>                    // for bool
 #include <ctype.h>                      // for isspace
 #include <errno.h>                      // for errno
 #include <event2/event.h>               // for event_base_dispatch, etc
 #include <fcntl.h>                      // for open
-#include <openssl/rand.h>               // for RAND_bytes
 #include <signal.h>                     // for signal, SIG_IGN, SIGHUP, etc
 #include <stddef.h>                     // for size_t
 #include <stdio.h>                      // for fprintf, NULL, fclose, etc
@@ -708,7 +709,10 @@ int main (int argc, char *argv[])
 	if (!preload_settings())
 		return 1;
 
-	RAND_bytes((unsigned char *)&instance_nonce, sizeof(instance_nonce));
+	if (prng_get_random_bytes((unsigned char *)&instance_nonce, sizeof(instance_nonce)) < 0) {
+		fprintf(stderr, "brd: no random data available\n");
+		return 1;
+	};
 
 	unsigned int arg;
 	for (arg = 1; arg < argc; arg++) {

src/wallet.c
@@ -12,6 +12,7 @@
 #include <ccoin/buffer.h>               // for const_buffer
 #include <ccoin/coredefs.h>             // for chain_info
 #include <ccoin/crypto/aes_util.h>      // for read_aes_file, etc
+#include <ccoin/crypto/prng.h>          // for prng_get_random_bytes
 #include <ccoin/cstr.h>                 // for cstring, cstr_free, etc
 #include <ccoin/hdkeys.h>               // for hd_extended_key_ser_priv
 #include <ccoin/hexcode.h>              // for encode_hex
@@ -21,7 +22,6 @@
 #include <ccoin/compat.h>               // for parr_new
 
 #include <jansson.h>                    // for json_object_set_new, etc
-#include <openssl/rand.h>               // for RAND_bytes
 
 #include <assert.h>                     // for assert
 #include <stdbool.h>                    // for true, bool, false
@@ -196,7 +196,10 @@ void cur_wallet_create(void)
 	}
 
 	char seed[256];
-	RAND_bytes((unsigned char *) &seed[0], sizeof(seed));
+	if (prng_get_random_bytes((unsigned char *) &seed[0], sizeof(seed)) < 0) {
+		fprintf(stderr, "wallet: no random data available\n");
+		return;
+	}
 
 	char seed_str[(sizeof(seed) * 2) + 1];
 	encode_hex(seed_str, seed, sizeof(seed));

test/prng.c
@@ -1,0 +1,120 @@
+#include <ccoin/crypto/prng.h> // for prng_get_random_bytes
+
+#include <assert.h> // for assert
+#include <math.h>   // for M_PI, fabs
+#include <stdint.h> // for uint8_t
+#include <stdio.h>  // for printf, fprintf, stderr
+#include <stdlib.h> // for getenv
+
+#define SAMPLE_SIZE_BYTES 524288 // Multple of buffer
+#define BUFFER_SIZE 32
+
+#ifdef M_PI
+#define PI M_PI
+#else
+#define PI 3.14159265358979323846
+#endif
+
+extern double pochisq(const double ax, const int32_t df);
+extern void rt_init(int binmode);
+extern void rt_add(void *buf, int bufl);
+extern void rt_end(double *r_ent, double *r_chisq, double *r_mean,
+                   double *r_montepicalc, double *r_scc);
+
+int main(int argc, char *argv[]) {
+
+    if (!getenv("FORCE_PRNG_VERF")) {
+        fprintf(stderr,
+                "prng: Skipping PRNG verification test\n"
+                "prng: Set FORCE_PRNG_VERF=1 to enable PRNG verification\n");
+        return 77;
+    }
+
+    uint8_t ob[BUFFER_SIZE];
+    unsigned long totalc = 0; /* Total character count */
+    double montepi, chip, scc, ent, mean, chisq;
+
+    /* Initialise for calculations */
+
+    rt_init(0);
+
+    /* Scan input and count character occurrences */
+
+    for (totalc = 0; totalc < SAMPLE_SIZE_BYTES; totalc += BUFFER_SIZE) {
+        assert(prng_get_random_bytes(ob, BUFFER_SIZE) >= 0);
+        rt_add(ob, BUFFER_SIZE);
+    }
+
+    /* Complete calculation and return sequence metrics */
+
+    rt_end(&ent, &chisq, &mean, &montepi, &scc);
+
+    /* Calculate probability of observed distribution occurring from
+    the results of the Chi-Square test */
+
+    chip = pochisq(chisq, 255);
+
+    /* Print calculated results */
+    printf("Entropy:\n");
+    printf("========\n");
+    printf("Entropy = %f bits per byte.\n", ent);
+    printf("\nOptimum compression would reduce the size\n");
+    printf("of this %ld byte input by %d percent.\n\n", totalc,
+           (int16_t)(100 * (8 - ent) / 8.0));
+
+    // Optimum compression would reduction equal to 0%
+    assert((int16_t)(100 * (8 - ent) / 8.0) == 0);
+
+    printf("Chi-square Test:\n");
+    printf("================\n");
+    printf("Chi square distribution for %ld samples is %1.2f, and randomly\n",
+           totalc, chisq);
+    if (chip < 0.0001) {
+        printf(
+            "would exceed this value less than 0.01 percent of the times.\n\n");
+    } else if (chip > 0.9999) {
+        printf("would exceed this value more than than 99.99 percent of the "
+               "times.\n\n");
+    } else {
+        printf("would exceed this value %1.2f percent of the times.\n\n",
+               chip * 100);
+    }
+
+    // Chi-square test result between 10% and 90%
+    assert(90 > (chip * 100));
+    assert((chip * 100) > 10);
+
+    printf("Arithmetic Mean:\n");
+    printf("================\n");
+    printf("Arithmetic mean value of data bytes is %1.4f (%.1f = random).\n\n",
+           mean, 127.5);
+
+    // Arithmetic Mean between 127 and 128
+    assert(127.0 < mean);
+    assert(mean < 128.0);
+
+    printf("Monte Carlo Value for Pi:\n");
+    printf("=========================\n");
+    printf("Monte Carlo value for Pi is %1.9f (error %1.2f percent).\n\n",
+           montepi, 100.0 * (fabs(PI - montepi) / PI));
+
+    // Monte Carlo Value for Pi less than 0.5
+    assert(0.5 > 100.0 * (fabs(PI - montepi) / PI));
+
+    printf("Serial Correlation Coefficient:\n");
+    printf("===============================\n");
+    printf("Serial correlation coefficient is ");
+    if (scc >= -99999) {
+        printf("%1.6f (totally uncorrelated = 0.0).\n", scc);
+    } else {
+        printf("undefined (all values equal!).\n");
+    }
+    printf("\nSee https://www.fourmilab.ch/random/ for detailed description of "
+           "output\n");
+
+    // Serial Correlation Coefficient between -0.005 and 0.005
+    assert(0.005 > scc);
+    assert(scc > -0.005);
+
+    return 0;
+}

libbitc/libbitc

lib/crypto/prng.c
@@ -1,0 +1,91 @@
+/*
+ * prng.c
+ *		Wrapper for builtin functions
+ *
+ * Copyright (c) 2001 Marko Kreen
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *	  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *	  notice, this list of conditions and the following disclaimer in the
+ *	  documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * contrib/pgcrypto/internal.c
+ */
+
+#include <bitc/crypto/fortuna.h> // for fortuna_add_entropy, etc
+#include <bitc/crypto/prng.h>
+
+#include <stdlib.h> // for NULL
+#include <string.h> // for memset
+#include <time.h>   // for time_t, time
+
+/*
+ * Randomness provider
+ */
+
+static time_t seed_time = 0;
+static time_t check_time = 0;
+
+static void system_reseed(void) {
+    uint8_t buf[1024];
+    int n;
+    time_t t;
+    int skip = 1;
+
+    t = time(NULL);
+
+    if (seed_time == 0)
+        skip = 0;
+    else if ((t - seed_time) < SYSTEM_RESEED_MIN)
+        skip = 1;
+    else if ((t - seed_time) > SYSTEM_RESEED_MAX)
+        skip = 0;
+    else if (check_time == 0 || (t - check_time) > SYSTEM_RESEED_CHECK_TIME) {
+        check_time = t;
+
+        /* roll dice */
+        prng_get_random_bytes(buf, 1);
+        skip = buf[0] >= SYSTEM_RESEED_CHANCE;
+    }
+    /* clear 1 byte */
+    memset(buf, 0, sizeof(buf));
+
+    if (skip)
+        return;
+
+    n = prng_acquire_system_randomness(buf);
+    if (n > 0)
+        fortuna_add_entropy(buf, n);
+
+    seed_time = t;
+    memset(buf, 0, sizeof(buf));
+}
+
+int prng_get_random_bytes(uint8_t *dst, unsigned count) {
+    system_reseed();
+    fortuna_get_bytes(count, dst);
+    return 0;
+}
+
+int prng_add_entropy(const uint8_t *data, unsigned count) {
+    system_reseed();
+    fortuna_add_entropy(data, count);
+    return 0;
+}

src/brd.c
@@ -10,6 +10,7 @@
 #include <bitc/clist.h>                // for clist_length
 #include <bitc/core.h>                 // for bitc_block, bitc_utxo, bitc_tx, etc
 #include <bitc/coredefs.h>             // for chain_info, chain_find, etc
+#include <bitc/crypto/prng.h>          // for prng_get_random_bytes
 #include <bitc/cstr.h>                 // for cstring, cstr_free
 #include <bitc/hexcode.h>              // for decode_hex
 #include <bitc/log.h>                  // for log_info, logging, etc
@@ -21,13 +22,13 @@
 #include <bitc/script.h>               // for bitc_verify_sig
 #include <bitc/util.h>                 // for ARRAY_SIZE, czstr_equal, etc
 
+
 #include <assert.h>                     // for assert
 #include <stdbool.h>                    // for bool
 #include <ctype.h>                      // for isspace
 #include <errno.h>                      // for errno
 #include <event2/event.h>               // for event_base_dispatch, etc
 #include <fcntl.h>                      // for open
-#include <openssl/rand.h>               // for RAND_bytes
 #include <signal.h>                     // for signal, SIG_IGN, SIGHUP, etc
 #include <stddef.h>                     // for size_t
 #include <stdio.h>                      // for fprintf, NULL, fclose, etc
@@ -708,7 +709,10 @@ int main (int argc, char *argv[])
 	if (!preload_settings())
 		return 1;
 
-	RAND_bytes((unsigned char *)&instance_nonce, sizeof(instance_nonce));
+	if (prng_get_random_bytes((unsigned char *)&instance_nonce, sizeof(instance_nonce)) < 0) {
+		fprintf(stderr, "brd: no random data available\n");
+		return 1;
+	};
 
 	unsigned int arg;
 	for (arg = 1; arg < argc; arg++) {

src/wallet.c
@@ -12,6 +12,7 @@
 #include <bitc/buffer.h>                // for const_buffer
 #include <bitc/coredefs.h>              // for chain_info
 #include <bitc/crypto/aes_util.h>       // for read_aes_file, etc
+#include <bitc/crypto/prng.h>          // for prng_get_random_bytes
 #include <bitc/hdkeys.h>                // for hd_extended_key_free, etc
 #include <bitc/hexcode.h>               // for encode_hex
 #include <bitc/key.h>                   // for bitc_privkey_get, etc
@@ -19,7 +20,6 @@
 #include <bitc/compat.h>                // for parr_new
 
 #include <jansson.h>                    // for json_object_set_new, etc
-#include <openssl/rand.h>               // for RAND_bytes
 
 #include <assert.h>                     // for assert
 #include <fcntl.h>                      // for open
@@ -192,7 +192,10 @@ void cur_wallet_create(void)
 	}
 
 	char seed[256];
-	RAND_bytes((unsigned char *) &seed[0], sizeof(seed));
+	if (prng_get_random_bytes((unsigned char *) &seed[0], sizeof(seed)) < 0) {
+		fprintf(stderr, "wallet: no random data available\n");
+		return;
+	}
 
 	char seed_str[(sizeof(seed) * 2) + 1];
 	encode_hex(seed_str, seed, sizeof(seed));

test/prng.c
@@ -1,0 +1,120 @@
+#include <bitc/crypto/prng.h> // for prng_get_random_bytes
+
+#include <assert.h> // for assert
+#include <math.h>   // for M_PI, fabs
+#include <stdint.h> // for uint8_t
+#include <stdio.h>  // for printf, fprintf, stderr
+#include <stdlib.h> // for getenv
+
+#define SAMPLE_SIZE_BYTES 524288 // Multple of buffer
+#define BUFFER_SIZE 32
+
+#ifdef M_PI
+#define PI M_PI
+#else
+#define PI 3.14159265358979323846
+#endif
+
+extern double pochisq(const double ax, const int32_t df);
+extern void rt_init(int binmode);
+extern void rt_add(void *buf, int bufl);
+extern void rt_end(double *r_ent, double *r_chisq, double *r_mean,
+                   double *r_montepicalc, double *r_scc);
+
+int main(int argc, char *argv[]) {
+
+    if (!getenv("FORCE_PRNG_VERF")) {
+        fprintf(stderr,
+                "prng: Skipping PRNG verification test\n"
+                "prng: Set FORCE_PRNG_VERF=1 to enable PRNG verification\n");
+        return 77;
+    }
+
+    uint8_t ob[BUFFER_SIZE];
+    unsigned long totalc = 0; /* Total character count */
+    double montepi, chip, scc, ent, mean, chisq;
+
+    /* Initialise for calculations */
+
+    rt_init(0);
+
+    /* Scan input and count character occurrences */
+
+    for (totalc = 0; totalc < SAMPLE_SIZE_BYTES; totalc += BUFFER_SIZE) {
+        assert(prng_get_random_bytes(ob, BUFFER_SIZE) >= 0);
+        rt_add(ob, BUFFER_SIZE);
+    }
+
+    /* Complete calculation and return sequence metrics */
+
+    rt_end(&ent, &chisq, &mean, &montepi, &scc);
+
+    /* Calculate probability of observed distribution occurring from
+    the results of the Chi-Square test */
+
+    chip = pochisq(chisq, 255);
+
+    /* Print calculated results */
+    printf("Entropy:\n");
+    printf("========\n");
+    printf("Entropy = %f bits per byte.\n", ent);
+    printf("\nOptimum compression would reduce the size\n");
+    printf("of this %ld byte input by %d percent.\n\n", totalc,
+           (int16_t)(100 * (8 - ent) / 8.0));
+
+    // Optimum compression would reduction equal to 0%
+    assert((int16_t)(100 * (8 - ent) / 8.0) == 0);
+
+    printf("Chi-square Test:\n");
+    printf("================\n");
+    printf("Chi square distribution for %ld samples is %1.2f, and randomly\n",
+           totalc, chisq);
+    if (chip < 0.0001) {
+        printf(
+            "would exceed this value less than 0.01 percent of the times.\n\n");
+    } else if (chip > 0.9999) {
+        printf("would exceed this value more than than 99.99 percent of the "
+               "times.\n\n");
+    } else {
+        printf("would exceed this value %1.2f percent of the times.\n\n",
+               chip * 100);
+    }
+
+    // Chi-square test result between 10% and 90%
+    assert(90 > (chip * 100));
+    assert((chip * 100) > 10);
+
+    printf("Arithmetic Mean:\n");
+    printf("================\n");
+    printf("Arithmetic mean value of data bytes is %1.4f (%.1f = random).\n\n",
+           mean, 127.5);
+
+    // Arithmetic Mean between 127 and 128
+    assert(127.0 < mean);
+    assert(mean < 128.0);
+
+    printf("Monte Carlo Value for Pi:\n");
+    printf("=========================\n");
+    printf("Monte Carlo value for Pi is %1.9f (error %1.2f percent).\n\n",
+           montepi, 100.0 * (fabs(PI - montepi) / PI));
+
+    // Monte Carlo Value for Pi less than 0.5
+    assert(0.5 > 100.0 * (fabs(PI - montepi) / PI));
+
+    printf("Serial Correlation Coefficient:\n");
+    printf("===============================\n");
+    printf("Serial correlation coefficient is ");
+    if (scc >= -99999) {
+        printf("%1.6f (totally uncorrelated = 0.0).\n", scc);
+    } else {
+        printf("undefined (all values equal!).\n");
+    }
+    printf("\nSee https://www.fourmilab.ch/random/ for detailed description of "
+           "output\n");
+
+    // Serial Correlation Coefficient between -0.005 and 0.005
+    assert(0.005 > scc);
+    assert(scc > -0.005);
+
+    return 0;
+}