aido/picocoin

db: Modularize database code

lib/net/net.c
@@ -6,7 +6,7 @@
 
 #include "ccoin/net/net.h"              // for nc_conn, net_child_info, etc
 #include <ccoin/net/netbase.h>          // for bn_address_str, etc
-#include <ccoin/blkdb.h>                // for blkdb, blkdb_locator, etc
+#include <ccoin/db/blkdb.h>             // for blkdb, blkdb_locator, etc
 #include <ccoin/buffer.h>               // for buffer, const_buffer
 #include <ccoin/core.h>                 // for bp_address, bp_inv, etc
 #include <ccoin/coredefs.h>             // for ::CADDR_TIME_VERSION, etc

src/brd.c
@@ -5,7 +5,7 @@
 #include "picocoin-config.h"           // for VERSION, _LARGE_FILES, etc
 
 #include "brd.h"
-#include <ccoin/blkdb.h>                // for blkinfo, blkdb, etc
+#include <ccoin/db/blkdb.h>             // for blkinfo, blkdb, etc
 #include <ccoin/buffer.h>               // for const_buffer, buffer_copy, etc
 #include <ccoin/clist.h>                // for clist_length
 #include <ccoin/core.h>                 // for bp_block, bp_utxo, bp_tx, etc

libbitc/libbitc

lib/net/net.c
@@ -4,9 +4,9 @@
  */
 #include "libbitc-config.h"            // for VERSION
 
-#include "bitc/net/net.h"              // for nc_conn, net_child_info, etc
+#include <bitc/net/net.h>              // for nc_conn, net_child_info, etc
 #include <bitc/net/netbase.h>          // for bn_address_str, etc
-#include <bitc/blkdb.h>                // for blkdb, blkdb_locator, etc
+#include <bitc/db/blkdb.h>             // for blkdb, blkdb_locator, etc
 #include <bitc/buffer.h>               // for buffer, const_buffer
 #include <bitc/core.h>                 // for bitc_address, bitc_inv, etc
 #include <bitc/coredefs.h>             // for ::CADDR_TIME_VERSION, etc

src/brd.c
@@ -5,7 +5,7 @@
 #include "libbitc-config.h"           // for VERSION, _LARGE_FILES, etc
 
 #include "brd.h"
-#include <bitc/blkdb.h>                // for blkinfo, blkdb, etc
+#include <bitc/db/blkdb.h>             // for blkinfo, blkdb, etc
 #include <bitc/buffer.h>               // for const_buffer, buffer_copy, etc
 #include <bitc/clist.h>                // for clist_length
 #include <bitc/core.h>                 // for bitc_block, bitc_utxo, bitc_tx, etc