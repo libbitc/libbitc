aido/picocoin

db: Change in memory blkdb database to use data from LMDB databases instead of from file Also rename blkdb database to chaindb to avoid confusion with LMDB blockdb

lib/net/net.c
@@ -6,7 +6,7 @@
 
 #include "ccoin/net/net.h"              // for nc_conn, net_child_info, etc
 #include <ccoin/net/netbase.h>          // for bn_address_str, etc
-#include <ccoin/db/blkdb.h>             // for blkdb, blkdb_locator, etc
+#include <ccoin/db/chaindb.h>           // for chaindb, chaindb_locator, etc
 #include <ccoin/buffer.h>               // for buffer, const_buffer
 #include <ccoin/core.h>                 // for bp_address, bp_inv, etc
 #include <ccoin/coredefs.h>             // for ::CADDR_TIME_VERSION, etc
@@ -317,7 +317,7 @@ static bool nc_msg_verack(struct nc_conn *conn)
 	if (conn->nci->last_getblocks < cutoff) {
 		struct msg_getblocks gb;
 		msg_getblocks_init(&gb);
-		blkdb_locator(conn->nci->db, NULL, &gb.locator);
+		chaindb_locator(conn->nci->db, NULL, &gb.locator);
 		cstring *s = ser_msg_getblocks(&gb);
 
 		rc = nc_conn_send(conn, "getblocks", s->str, s->len);

src/brd.c
@@ -5,7 +5,7 @@
 #include "picocoin-config.h"           // for VERSION, _LARGE_FILES, etc
 
 #include "brd.h"
-#include <ccoin/db/blkdb.h>             // for blkinfo, blkdb, etc
+#include <ccoin/db/chaindb.h>           // for blkinfo, chaindb, etc
 #include <ccoin/db/db.h>                // for blockdb_add, blockdb_init, etc
 #include <ccoin/buffer.h>               // for const_buffer, buffer_copy, etc
 #include <ccoin/clist.h>                // for clist_length
@@ -50,7 +50,7 @@ uint64_t instance_nonce;
 struct logging *log_state;
 bool debugging = false;
 
-static struct blkdb db;
+static struct chaindb db;
 static struct bp_hashtab *orphans;
 static struct bp_utxo_set uset;
 static bool script_verf = false;
@@ -61,7 +61,6 @@ static const char *const_settings[] = {
 	"net.connect.timeout=11",
 	"chain=bitcoin",
 	"peers=brd.peers",
-	/* "blkdb=brd.blkdb", */
 	"log=-", /* "log=brd.log", */
 };
 
@@ -202,33 +201,16 @@ static void init_log(void)
 
 }
 
-static void init_blkdb(void)
+static void init_chaindb(void)
 {
 	char hexstr[BU256_STRSZ];
 
-	if (!blkdb_init(&db, chain->netmagic, &chain_genesis)) {
-		log_info("%s: blkdb init failed", prog_name);
+	if (!chaindb_init(&db, chain->netmagic, &chain_genesis)) {
+		log_info("%s: chaindb initialisation failed", prog_name);
 		exit(1);
 	}
 
-	char *blkdb_fn = setting("blkdb");
-	if (!blkdb_fn)
-		return;
-
-	if ((access(blkdb_fn, F_OK) == 0) &&
-	    !blkdb_read(&db, blkdb_fn)) {
-		log_info("%s: blkdb read failed", prog_name);
-		exit(1);
-	}
-
-	db.fd = open(blkdb_fn,
-		     O_WRONLY | O_APPEND | O_CREAT | O_LARGEFILE, 0666);
-	if (db.fd < 0) {
-		log_error("%s: blkdb file open failed: %s", prog_name, strerror(errno));
-		exit(1);
-	}
-
-    log_debug("%s: blkdb opened", prog_name);
+    log_debug("%s: Initialised chaindb", prog_name);
 }
 
 static void init_db(void)
@@ -237,7 +219,7 @@ static void init_db(void)
 		!blockdb_init() ||
 		!blockheightdb_init())
 		{
-		log_error("%s: db init failed", prog_name);
+		log_error("%s: db initialisation failed", prog_name);
 		exit(1);
 	}
 
@@ -376,14 +358,13 @@ static bool block_process(const struct bp_block *block)
 	struct blkinfo *bi = bi_new();
 	bu256_copy(&bi->hash, &block->sha256);
 	bp_block_copy_hdr(&bi->hdr, block);
-	bi->n_file = 0;
 	char hexstr[BU256_STRSZ];
 	bu256_hex(hexstr, &bi->hash);
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	if (!blkdb_add(&db, bi, &reorg)) {
-		log_debug("%s: Adding block %s to blkdb failed", prog_name, hexstr);
+	if (!chaindb_add(&db, bi, &reorg)) {
+		log_debug("%s: Adding block %s to chaindb failed", prog_name, hexstr);
 		goto err_out;
 	}
 
@@ -398,7 +379,7 @@ static bool block_process(const struct bp_block *block)
 			log_info("%s: block spend fail %u %s",
 				prog_name,
 				bi->height, hexstr);
-			/* FIXME: bad record is now in blkdb */
+			/* FIXME: bad record is now in chaindb */
 			goto err_out;
 		}
 	}
@@ -506,14 +487,14 @@ static void init_peers(struct net_child_info *nci)
 
 static bool inv_block_process(bu256_t *hash)
 {
-    return (!blkdb_lookup(&db, hash) &&
+    return (!chaindb_lookup(&db, hash) &&
 			    !have_orphan(hash));
 }
 
 static bool add_block(struct bp_block *block, struct const_buffer *buf)
 {
     /* check for duplicate block */
-    if (blkdb_lookup(&db, &block->sha256) ||
+    if (chaindb_lookup(&db, &block->sha256) ||
         have_orphan(&block->sha256))
         return true;
 
@@ -545,12 +526,11 @@ static void init_nci(struct net_child_info *nci)
 
 static void init_daemon(struct net_child_info *nci)
 {
-	init_blkdb();
+	init_chaindb();
 	bp_utxo_set_init(&uset);
 	init_block0();
 	init_orphans();
-	if (db.fd < 0)
-		blockheightdb_getall(read_block);
+	blockheightdb_getall(read_block);
 	init_nci(nci);
 }
 
@@ -592,7 +572,7 @@ static void shutdown_daemon(struct net_child_info *nci)
 		shutdown_nci(nci);
 		bp_hashtab_unref(orphans);
 		bp_hashtab_unref(settings);
-		blkdb_free(&db);
+		chaindb_free(&db);
 		bp_utxo_set_free(&uset);
 	}
 }

test/chain-verf.c
@@ -1,24 +1,34 @@
-#include "picocoin-config.h"
+/* Copyright 2012 exMULTI, Inc.
+ * Distributed under the MIT/X11 software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.
+ */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <assert.h>
-#include <ccoin/coredefs.h>
-#include <ccoin/message.h>
-#include <ccoin/mbr.h>
-#include <ccoin/db/blkdb.h>
-#include <ccoin/script.h>
-#include <ccoin/util.h>
-#include <ccoin/checkpoints.h>
-#include "libtest.h"
+#include <ccoin/buffer.h>               // for const_buffer
+#include <ccoin/buint.h>                // for bu256_hex, BU256_STRSZ, etc
+#include <ccoin/checkpoints.h>          // for bp_ckpt_last
+#include <ccoin/core.h>                 // for bp_block, bp_tx, bp_utxo, etc
+#include <ccoin/coredefs.h>             // for chain_info, etc
+#include <ccoin/db/chaindb.h>           // for blkinfo, chaindb_reorg, etc
+#include <ccoin/db/db.h>                // for blockdb_init, etc
+#include <ccoin/key.h>                  // for bp_key_static_shutdown
+#include <ccoin/log.h>                  // for logging
+#include <ccoin/mbr.h>                  // for fread_block
+#include <ccoin/message.h>              // for p2p_message, etc
+#include <ccoin/parr.h>                 // for parr, parr_id
+#include <ccoin/script.h>               // for bp_verify_sig
+#include <ccoin/util.h>                 // for file_seq_open
+
+#include <assert.h>                     // for assert
+#include <stdbool.h>                    // for true, false, bool
+#include <stdint.h>                     // for int64_t
+#include <stdio.h>                      // for fprintf, stderr, perror, etc
+#include <stdlib.h>                     // for getenv, calloc, free
+#include <string.h>                     // for memcmp, strncmp
+#include <unistd.h>                     // for close
 
 static bool no_script_verf = false;
 static bool force_script_verf = false;
+struct logging *log_state;
 
 static bool spend_tx(struct bp_utxo_set *uset, const struct bp_tx *tx,
 		     unsigned int tx_idx, unsigned int height,
@@ -124,7 +134,7 @@ static bool spend_block(struct bp_utxo_set *uset, const struct bp_block *block,
 	return true;
 }
 
-static void read_test_msg(struct blkdb *db, struct bp_utxo_set *uset,
+static void read_test_msg(struct chaindb *db, struct bp_utxo_set *uset,
 			  const struct p2p_message *msg, int64_t fpos,
 			  unsigned int ckpt_height)
 {
@@ -143,12 +153,10 @@ static void read_test_msg(struct blkdb *db, struct bp_utxo_set *uset,
 	struct blkinfo *bi = bi_new();
 	bu256_copy(&bi->hash, &block.sha256);
 	bp_block_copy_hdr(&bi->hdr, &block);
-	bi->n_file = 0;
-	bi->n_pos = fpos + P2P_HDR_SZ;
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	assert(blkdb_add(db, bi, &reorg) == true);
+	assert(chaindb_add(db, bi, &reorg) == true);
 
 	assert(reorg.conn == 1);
 	assert(reorg.disconn == 0);
@@ -175,11 +183,15 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 
 	unsigned int ckpt_height = bp_ckpt_last(chain_id);
 
-	struct blkdb blkdb;
+	struct chaindb chaindb;
 	bu256_t blk0;
 
 	hex_bu256(&blk0, chain->genesis_hash);
-	assert(blkdb_init(&blkdb, chain->netmagic, &blk0) == true);
+	assert(metadb_init(chain_metadata[chain_id].netmagic, (const bu256_t *)chain_metadata[chain_id].genesis_hash));
+	assert(blockdb_init());
+	assert(blockheightdb_init());
+
+	assert(chaindb_init(&chaindb, chain->netmagic, &blk0) == true);
 
 	struct bp_utxo_set uset;
 	bp_utxo_set_init(&uset);
@@ -203,7 +215,7 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 	while (fread_block(fd, &msg, &read_ok)) {
 		assert(memcmp(msg.hdr.netmagic, chain->netmagic, 4) == 0);
 
-		read_test_msg(&blkdb, &uset, &msg, fpos, ckpt_height);
+		read_test_msg(&chaindb, &uset, &msg, fpos, ckpt_height);
 
 		fpos += P2P_HDR_SZ;
 		fpos += msg.hdr.data_len;
@@ -215,7 +227,7 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 	close(fd);
 	free(msg.data);
 
-	blkdb_free(&blkdb);
+	chaindb_free(&chaindb);
 	bp_utxo_set_free(&uset);
 
 	fprintf(stderr, "chain-verf: %u records validated\n", records);
@@ -225,6 +237,12 @@ int main (int argc, char *argv[])
 {
 	char *fn;
 	unsigned int verfd = 0;
+
+	log_state = calloc(0, sizeof(struct logging));
+
+	log_state->stream = stderr;
+	log_state->logtofile = false;
+	log_state->debug = true;
 
 	if (getenv("NO_SCRIPT_VERF"))
 		no_script_verf = true;

test/chaindb.c
@@ -2,13 +2,12 @@
  * Distributed under the MIT/X11 software license, see the accompanying
  * file COPYING or http://www.opensource.org/licenses/mit-license.php.
  */
-#include "picocoin-config.h"
 
 #include <ccoin/buffer.h>               // for const_buffer
 #include <ccoin/buint.h>                // for hex_bu256, bu256_copy, etc
 #include <ccoin/core.h>                 // for bp_block_calc_sha256, etc
 #include <ccoin/coredefs.h>             // for chain_info, chain_metadata, etc
-#include <ccoin/db/blkdb.h>             // for blkinfo, blkdb, blkdb_reorg, etc
+#include <ccoin/db/chaindb.h>           // for blkinfo, chaindb, chaindb_reorg, etc
 #include <ccoin/db/db.h>                // for blockdb_init, etc
 #include <ccoin/key.h>                  // for bp_key_static_shutdown
 #include <ccoin/log.h>                  // for logging
@@ -23,7 +22,7 @@
 
 struct logging *log_state;
 
-static void add_header(struct blkdb *db, char *raw)
+static void add_header(struct chaindb *db, char *raw)
 {
 	struct const_buffer buf = { raw, 80 };
 
@@ -36,15 +35,15 @@ static void add_header(struct blkdb *db, char *raw)
 
 	bu256_copy(&bi->hash, &bi->hdr.sha256);
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	assert(blkdb_add(db, bi, &reorg) == true);
+	assert(chaindb_add(db, bi, &reorg) == true);
 
 	assert(reorg.conn == 1);
 	assert(reorg.disconn == 0);
 }
 
-static void read_headers(const char *ser_base_fn, struct blkdb *db)
+static void read_headers(const char *ser_base_fn, struct chaindb *db)
 {
 	char *filename = test_filename(ser_base_fn);
 	int fd = file_seq_open(filename);
@@ -60,7 +59,7 @@ static void read_headers(const char *ser_base_fn, struct blkdb *db)
 	free(filename);
 }
 
-static void test_blkinfo_prev(struct blkdb *db)
+static void test_blkinfo_prev(struct chaindb *db)
 {
 	struct blkinfo *tmp = db->best_chain;
 	int height = db->best_chain->height;
@@ -78,13 +77,13 @@ static void test_blkinfo_prev(struct blkdb *db)
 static void runtest(const char *ser_base_fn, const struct chain_info *chain,
 		    unsigned int check_height, const char *check_hash)
 {
-	struct blkdb db;
+	struct chaindb db;
 
 	bu256_t block0;
 	bool rc = hex_bu256(&block0, chain->genesis_hash);
 	assert(rc);
 
-	rc = blkdb_init(&db, chain->netmagic, &block0);
+	rc = chaindb_init(&db, chain->netmagic, &block0);
 	assert(rc);
 
 	read_headers(ser_base_fn, &db);
@@ -98,7 +97,7 @@ static void runtest(const char *ser_base_fn, const struct chain_info *chain,
 
 	test_blkinfo_prev(&db);
 
-	blkdb_free(&db);
+	chaindb_free(&db);
 }
 
 int main (int argc, char *argv[])

libbitc/libbitc

lib/net/net.c
@@ -6,7 +6,7 @@
 
 #include <bitc/net/net.h>              // for nc_conn, net_child_info, etc
 #include <bitc/net/netbase.h>          // for bn_address_str, etc
-#include <bitc/db/blkdb.h>             // for blkdb, blkdb_locator, etc
+#include <bitc/db/chaindb.h>           // for blkdb, blkdb_locator, etc
 #include <bitc/buffer.h>               // for buffer, const_buffer
 #include <bitc/core.h>                 // for bitc_address, bitc_inv, etc
 #include <bitc/coredefs.h>             // for ::CADDR_TIME_VERSION, etc
@@ -318,7 +318,7 @@ static bool nc_msg_verack(struct nc_conn *conn)
 	if (conn->nci->last_getblocks < cutoff) {
 		struct msg_getblocks gb;
 		msg_getblocks_init(&gb);
-		blkdb_locator(conn->nci->db, NULL, &gb.locator);
+		chaindb_locator(conn->nci->db, NULL, &gb.locator);
 		cstring *s = ser_msg_getblocks(&gb);
 
 		rc = nc_conn_send(conn, "getblocks", s->str, s->len);

src/brd.c
@@ -5,7 +5,7 @@
 #include "libbitc-config.h"           // for VERSION, _LARGE_FILES, etc
 
 #include "brd.h"
-#include <bitc/db/blkdb.h>             // for blkinfo, blkdb, etc
+#include <bitc/db/chaindb.h>           // for blkinfo, blkdb, etc
 #include <bitc/db/db.h>                // for blockdb_init, db_close, etc
 #include <bitc/buffer.h>               // for const_buffer, buffer_copy, etc
 #include <bitc/clist.h>                // for clist_length
@@ -51,7 +51,7 @@ uint64_t instance_nonce;
 struct logging *log_state;
 bool debugging = false;
 
-static struct blkdb db;
+static struct chaindb db;
 static struct bitc_hashtab *orphans;
 static struct bitc_utxo_set uset;
 static bool script_verf = false;
@@ -62,7 +62,6 @@ static const char *const_settings[] = {
 	"net.connect.timeout=11",
 	"chain=bitcoin",
 	"peers=brd.peers",
-	/* "blkdb=brd.blkdb", */
 	"log=-", /* "log=brd.log", */
 };
 
@@ -203,33 +202,16 @@ static void init_log(void)
 
 }
 
-static void init_blkdb(void)
+static void init_chaindb(void)
 {
 	char hexstr[BU256_STRSZ];
 
-	if (!blkdb_init(&db, chain->netmagic, &chain_genesis)) {
-		log_info("%s: blkdb init failed", prog_name);
+	if (!chaindb_init(&db, chain->netmagic, &chain_genesis)) {
+		log_info("%s: chaindb initialisation failed", prog_name);
 		exit(1);
 	}
 
-	char *blkdb_fn = setting("blkdb");
-	if (!blkdb_fn)
-		return;
-
-	if ((access(blkdb_fn, F_OK) == 0) &&
-	    !blkdb_read(&db, blkdb_fn)) {
-		log_info("%s: blkdb read failed", prog_name);
-		exit(1);
-	}
-
-	db.fd = open(blkdb_fn,
-		     O_WRONLY | O_APPEND | O_CREAT | O_LARGEFILE, 0666);
-	if (db.fd < 0) {
-		log_error("%s: blkdb file open failed: %s", prog_name, strerror(errno));
-		exit(1);
-	}
-
-    log_debug("%s: blkdb opened", prog_name);
+    log_debug("%s: Initialised chaindb", prog_name);
 }
 
 static void init_db(void)
@@ -238,7 +220,7 @@ static void init_db(void)
 		!blockdb_init() ||
 		!blockheightdb_init())
 		{
-		log_error("%s: db init failed", prog_name);
+		log_error("%s: db initialisation failed", prog_name);
 		exit(1);
 	}
 
@@ -376,14 +358,13 @@ static bool block_process(const struct bitc_block *block)
 	struct blkinfo *bi = bi_new();
 	bu256_copy(&bi->hash, &block->sha256);
 	bitc_block_copy_hdr(&bi->hdr, block);
-	bi->n_file = 0;
 	char hexstr[BU256_STRSZ];
 	bu256_hex(hexstr, &bi->hash);
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	if (!blkdb_add(&db, bi, &reorg)) {
-		log_debug("%s: Adding block %s to blkdb failed", prog_name, hexstr);
+	if (!chaindb_add(&db, bi, &reorg)) {
+		log_debug("%s: Adding block %s to chaindb failed", prog_name, hexstr);
 		goto err_out;
 	}
 
@@ -399,7 +380,7 @@ static bool block_process(const struct bitc_block *block)
 			log_info("%s: block spend fail %u %s",
 				prog_name,
 				bi->height, hexstr);
-			/* FIXME: bad record is now in blkdb */
+			/* FIXME: bad record is now in chaindb */
 			goto err_out;
 		}
 	}
@@ -507,14 +488,14 @@ static void init_peers(struct net_child_info *nci)
 
 static bool inv_block_process(bu256_t *hash)
 {
-    return (!blkdb_lookup(&db, hash) &&
+    return (!chaindb_lookup(&db, hash) &&
 			    !have_orphan(hash));
 }
 
 static bool add_block(struct bitc_block *block, struct const_buffer *buf)
 {
     /* check for duplicate block */
-    if (blkdb_lookup(&db, &block->sha256) ||
+    if (chaindb_lookup(&db, &block->sha256) ||
         have_orphan(&block->sha256))
         return true;
 
@@ -546,12 +527,11 @@ static void init_nci(struct net_child_info *nci)
 
 static void init_daemon(struct net_child_info *nci)
 {
-	init_blkdb();
+	init_chaindb();
 	bitc_utxo_set_init(&uset);
 	init_block0();
 	init_orphans();
-	if (db.fd < 0)
-		blockheightdb_getall(read_block);
+	blockheightdb_getall(read_block);
 	init_nci(nci);
 }
 
@@ -593,7 +573,7 @@ static void shutdown_daemon(struct net_child_info *nci)
 		shutdown_nci(nci);
 		bitc_hashtab_unref(orphans);
 		bitc_hashtab_unref(settings);
-		blkdb_free(&db);
+		chaindb_free(&db);
 		bitc_utxo_set_free(&uset);
 	}
 }

test/chain-verf.c
@@ -5,14 +5,16 @@
 
 #include "libbitc-config.h"
 
-#include <bitc/db/blkdb.h>              // for blkinfo, blkdb_reorg, etc
 #include <bitc/buffer.h>                // for const_buffer
 #include <bitc/buint.h>                 // for bu256_hex, BU256_STRSZ, etc
+#include <bitc/db/chaindb.h>            // for blkinfo, chaindb_reorg, etc
+#include <bitc/db/db.h>                 // for blockdb_init, etc
 #include <bitc/core.h>                  // for bitc_block, bitc_tx, etc
 #include <bitc/key.h>                   // for bitc_key_static_shutdown
 #include <bitc/parr.h>                  // for parr, parr_idx
 #include <bitc/checkpoints.h>           // for bitc_ckpt_last
 #include <bitc/coredefs.h>              // for chain_info, etc
+#include <bitc/log.h>                   // for logging
 #include <bitc/mbr.h>                   // for fread_block
 #include <bitc/message.h>               // for p2p_message, etc
 #include <bitc/script.h>                // for bitc_verify_sig, etc
@@ -29,6 +31,7 @@
 
 static bool no_script_verf = false;
 static bool force_script_verf = false;
+struct logging *log_state;
 
 static bool spend_tx(struct bitc_utxo_set *uset, const struct bitc_tx *tx,
 		     unsigned int tx_idx, unsigned int height,
@@ -133,7 +136,7 @@ static bool spend_block(struct bitc_utxo_set *uset, const struct bitc_block *blo
 	return true;
 }
 
-static void read_test_msg(struct blkdb *db, struct bitc_utxo_set *uset,
+static void read_test_msg(struct chaindb *db, struct bitc_utxo_set *uset,
 			  const struct p2p_message *msg, int64_t fpos,
 			  unsigned int ckpt_height)
 {
@@ -152,12 +155,10 @@ static void read_test_msg(struct blkdb *db, struct bitc_utxo_set *uset,
 	struct blkinfo *bi = bi_new();
 	bu256_copy(&bi->hash, &block.sha256);
 	bitc_block_copy_hdr(&bi->hdr, &block);
-	bi->n_file = 0;
-	bi->n_pos = fpos + P2P_HDR_SZ;
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	assert(blkdb_add(db, bi, &reorg) == true);
+	assert(chaindb_add(db, bi, &reorg) == true);
 
 	assert(reorg.conn == 1);
 	assert(reorg.disconn == 0);
@@ -184,11 +185,15 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 
 	unsigned int ckpt_height = bitc_ckpt_last(chain_id);
 
-	struct blkdb blkdb;
+	struct chaindb chaindb;
 	bu256_t blk0;
 
 	hex_bu256(&blk0, chain->genesis_hash);
-	assert(blkdb_init(&blkdb, chain->netmagic, &blk0) == true);
+	assert(metadb_init(chain_metadata[chain_id].netmagic, (const bu256_t *)chain_metadata[chain_id].genesis_hash));
+	assert(blockdb_init());
+	assert(blockheightdb_init());
+
+	assert(chaindb_init(&chaindb, chain->netmagic, &blk0) == true);
 
 	struct bitc_utxo_set uset;
 	bitc_utxo_set_init(&uset);
@@ -212,7 +217,7 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 	while (fread_block(fd, &msg, &read_ok)) {
 		assert(memcmp(msg.hdr.netmagic, chain->netmagic, 4) == 0);
 
-		read_test_msg(&blkdb, &uset, &msg, fpos, ckpt_height);
+		read_test_msg(&chaindb, &uset, &msg, fpos, ckpt_height);
 
 		fpos += P2P_HDR_SZ;
 		fpos += msg.hdr.data_len;
@@ -224,7 +229,7 @@ static void runtest(bool use_testnet, const char *blocks_fn)
 	close(fd);
 	free(msg.data);
 
-	blkdb_free(&blkdb);
+	chaindb_free(&chaindb);
 	bitc_utxo_set_free(&uset);
 
 	fprintf(stderr, "chain-verf: %u records validated\n", records);
@@ -234,6 +239,12 @@ int main (int argc, char *argv[])
 {
 	char *fn;
 	unsigned int verfd = 0;
+
+	log_state = calloc(0, sizeof(struct logging));
+
+	log_state->stream = stderr;
+	log_state->logtofile = false;
+	log_state->debug = true;
 
 	if (getenv("NO_SCRIPT_VERF"))
 		no_script_verf = true;

test/chaindb.c
@@ -6,9 +6,10 @@
 
 #include <bitc/buffer.h>                // for const_buffer
 #include <bitc/buint.h>                 // for hex_bu256, bu256_copy, etc
+#include <bitc/db/chaindb.h>            // for blkinfo, blkdb, blkdb_reorg, etc
+#include <bitc/db/db.h>                 // for blockdb_init, etc
 #include <bitc/core.h>                  // for bitc_block_calc_sha256, etc
 #include <bitc/coredefs.h>              // for chain_info, chain_metadata, etc
-#include <bitc/db/blkdb.h>              // for blkinfo, blkdb, blkdb_reorg, etc
 #include <bitc/key.h>                   // for bitc_key_static_shutdown
 #include <bitc/log.h>                   // for logging
 #include <bitc/util.h>                  // for file_seq_open
@@ -19,7 +20,7 @@
 #include <stdlib.h>                     // for free, NULL
 #include <unistd.h>                     // for close, read
 
-static void add_header(struct blkdb *db, char *raw)
+static void add_header(struct chaindb *db, char *raw)
 {
 	struct const_buffer buf = { raw, 80 };
 
@@ -32,15 +33,15 @@ static void add_header(struct blkdb *db, char *raw)
 
 	bu256_copy(&bi->hash, &bi->hdr.sha256);
 
-	struct blkdb_reorg reorg;
+	struct chaindb_reorg reorg;
 
-	assert(blkdb_add(db, bi, &reorg) == true);
+	assert(chaindb_add(db, bi, &reorg) == true);
 
 	assert(reorg.conn == 1);
 	assert(reorg.disconn == 0);
 }
 
-static void read_headers(const char *ser_base_fn, struct blkdb *db)
+static void read_headers(const char *ser_base_fn, struct chaindb *db)
 {
 	char *filename = test_filename(ser_base_fn);
 	int fd = file_seq_open(filename);
@@ -56,7 +57,7 @@ static void read_headers(const char *ser_base_fn, struct blkdb *db)
 	free(filename);
 }
 
-static void test_blkinfo_prev(struct blkdb *db)
+static void test_blkinfo_prev(struct chaindb *db)
 {
 	struct blkinfo *tmp = db->best_chain;
 	int height = db->best_chain->height;
@@ -74,13 +75,13 @@ static void test_blkinfo_prev(struct blkdb *db)
 static void runtest(const char *ser_base_fn, const struct chain_info *chain,
 		    unsigned int check_height, const char *check_hash)
 {
-	struct blkdb db;
+	struct chaindb db;
 
 	bu256_t block0;
 	bool rc = hex_bu256(&block0, chain->genesis_hash);
 	assert(rc);
 
-	rc = blkdb_init(&db, chain->netmagic, &block0);
+	rc = chaindb_init(&db, chain->netmagic, &block0);
 	assert(rc);
 
 	read_headers(ser_base_fn, &db);
@@ -94,7 +95,7 @@ static void runtest(const char *ser_base_fn, const struct chain_info *chain,
 
 	test_blkinfo_prev(&db);
 
-	blkdb_free(&db);
+	chaindb_free(&db);
 }
 
 int main (int argc, char *argv[])
@@ -105,13 +106,13 @@ int main (int argc, char *argv[])
 	log_state->logtofile = false;
 	log_state->debug = true;
 
-	assert(metadb_init(chain_metadata[CHAIN_BITCOIN].netmagic, chain_metadata[CHAIN_BITCOIN].genesis_hash));
+	assert(metadb_init(chain_metadata[CHAIN_BITCOIN].netmagic, (const bu256_t *)chain_metadata[CHAIN_BITCOIN].genesis_hash));
 	assert(blockdb_init());
 	assert(blockheightdb_init());
 	runtest("data/hdr193000.ser", &chain_metadata[CHAIN_BITCOIN], 193000,
 	    "000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317");
 
-	assert(metadb_init(chain_metadata[CHAIN_TESTNET3].netmagic, chain_metadata[CHAIN_TESTNET3].genesis_hash));
+	assert(metadb_init(chain_metadata[CHAIN_TESTNET3].netmagic, (const bu256_t *)chain_metadata[CHAIN_TESTNET3].genesis_hash));
 	assert(blockdb_init());
 	assert(blockheightdb_init());
 	runtest("data/tn_hdr35141.ser", &chain_metadata[CHAIN_TESTNET3], 35141,